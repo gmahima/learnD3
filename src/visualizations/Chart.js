4a20c346e8d5854731623bc7952bdfe08be6d2d3
Mahima Gangavarapu <mahimagangavarapu@Mahimas-MacBook-Air.local>
2020-01-16 19:37:16 +0530

    bar chart

--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -6,97 +6,66 @@ const height = 450;
 const margin = { top: 20, right: 5, bottom: 20, left: 35 };
 
 class Chart extends Component {
-//   state = {
-//     bars: [],
-//     radial: []
-//   };
-//  xAxis = d3.axisLeft();
-//  yAxis = d3.axisBottom();
+  state = {
+    bars: [],
+    radial: []
+  };
+ xAxis = d3.axisLeft();
+ yAxis = d3.axisBottom();
 
-//   static getDerivedStateFromProps(nextProps, prevState) {
-//     const { data } = nextProps;
-//     if (!data) return {};
-//     const extent = d3.extent(data, d=> d.date)
-//     const xScale = d3.scaleTime()
-//       .domain(extent)
-//       .range([margin.left, width-margin.right])
-//     // console.log('===', xScale(new Date('12/31/2017')  ))
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { data } = nextProps;
+    if (!data) return {};
+    const extent = d3.extent(data, d=> d.date)
+    const xScale = d3.scaleTime()
+      .domain(extent)
+      .range([margin.left, width-margin.right])
+    // console.log('===', xScale(new Date('12/31/2017')  ))
     
-//     const [min, max] = d3.extent(data, d=> d.high)
-//     const yScale = d3.scaleLinear()
-//       .domain([Math.min(min, 0), max])
-//       .range([height-margin.bottom, margin.top])
+    const [min, max] = d3.extent(data, d=> d.high)
+    const yScale = d3.scaleLinear()
+      .domain([Math.min(min, 0), max])
+      .range([height-margin.bottom, margin.top])
     
-//     // const highEx = d3.extent(data, d=>d.high)
-//     // const lowEx = d3.extent(data, d=>d.low)
-//     // console.log('high range', highEx, 'low range', lowEx)
+    // const highEx = d3.extent(data, d=>d.high)
+    // const lowEx = d3.extent(data, d=>d.low)
+    // console.log('high range', highEx, 'low range', lowEx)
     
-//     const colorExtent = d3.extent(data, d=>d.avg).reverse()
-//     //const colorExtent = d3.extent(data.map(d=>d.avg)).reverse()
-//     const colorScale = d3.scaleSequential()
-//   .domain(colorExtent).interpolator(d3.interpolateRdYlBu)
-//     console.log(d3.extent(data.map(d=>d.avg)).reverse())
-//     console.log(colorExtent)
+    const colorExtent = d3.extent(data, d=>d.avg).reverse()
+    //const colorExtent = d3.extent(data.map(d=>d.avg)).reverse()
+    const colorScale = d3.scaleSequential()
+  .domain(colorExtent).interpolator(d3.interpolateRdYlBu)
+    console.log(d3.extent(data.map(d=>d.avg)).reverse())
+    console.log(colorExtent)
     
-//    const bars= data.map(d=> {
-//     return {
-//       x: xScale(d.date),
-//       y: yScale(d.high),
-//       height: yScale(d.low) - yScale(d.high),
-//       fill: colorScale(d.avg),
-//     }
-//    })
-//     return {bars, xScale, yScale};
-//   }
-//   componentDidUpdate(){
+   const bars= data.map(d=> {
+    return {
+      x: xScale(d.date),
+      y: yScale(d.high),
+      height: yScale(d.low) - yScale(d.high),
+      fill: colorScale(d.avg),
+    }
+   })
+    return {bars, xScale, yScale};
+  }
+  componentDidUpdate(){
     
-//   }
-//   render() {
+  }
+  render() {
 
-//     return (
-//     <svg width={width} height={height} >
+    return (
+    <svg width={width} height={height} >
 
-//         {
-//           this.state.bars.map(d=>{
-//             return <rect x={d.x} y={d.y} width={2} height={d.height}fill={d.fill}  />
-//           })
-//         }
-//         <g ref={'xAxis'}/>
-//         <g ref={'yAxis'}/>
+        {
+          this.state.bars.map(d=>{
+            return <rect x={d.x} y={d.y} width={2} height={d.height}fill={d.fill}  />
+          })
+        }
+        <g ref={'xAxis'}/>
+        <g ref={'yAxis'}/>
 
 
-//    </svg>)
-//   }
-myChart = Highcharts.chart('container', {
-  chart: {
-      type: 'bar'
-  },
-  title: {
-      text: 'Fruit Consumption'
-  },
-  xAxis: {
-      categories: ['Apples', 'Bananas', 'Oranges']
-  },
-  yAxis: {
-      title: {
-          text: 'Fruit eaten'
-      }
-  },
-  series: [{
-      name: 'Jane',
-      data: [1, 0, 4]
-  }, {
-      name: 'John',
-      data: [5, 7, 3]
-  }]
-});
-render() {
-
-  return <div>
-    {this.myChart}
-  </div>
+   </svg>)
+  }
 }
-}
-
-
 export default Chart;
